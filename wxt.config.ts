dbeaeb161be0c8e0206cefe1e4dcdeee0d900b52
Shirat0ri <shiratori.ivan@gmail.com>
2026-02-15 14:11:32 +0800

    Update configuration for GCrash extension

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,18 +4,18 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     zip: {
-        name: "microslop",
+        name: "gcrash",
         excludeSources: ["debub.log", "bun.lock"],
     },
     manifest: {
         version: "0.0.5",
-        name: "Microslop",
-        short_name: "Microslop",
-        description: "An extension that converts Microsoft to Microslop",
+        name: "GCrasg",
+        short_name: "gcrash_meme",
+        description: "An extension that converts GCash to GCrash",
         host_permissions: ["*://*/*"],
         browser_specific_settings: {
             gecko: {
-                id: "microslop@4o4",
+                id: "gcrash@4o4",
                 strict_min_version: "109.0",
                 ["data_collection_permissions" as any]: {
                     required: ["none"],
